01dc02e2444f5ed9955c6d70132300f73c9fa5c1
Alex Witkowski <alex@sibipro.com>
2019-02-25 10:31:23 -0700

    updated file names and references

--- server.js
+++ server.js
@@ -2,23 +2,22 @@ var express = require('express'),
   app = express(),
   port = process.env.PORT || 3000,
   mongoose = require('mongoose'),
-  Task = require('./api/models/todoListModel'), //created model loading here
+  Task = require('./api/models/model'), //created model loading here
   bodyParser = require('body-parser');
   
 // mongoose instance connection url connection
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost/Tododb'); 
 
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 
-var routes = require('./api/routes/todoListRoutes'); //importing route
+var routes = require('./api/routes/routes'); //importing route
 routes(app); //register the route
 
-app.listen(port, 40, (callback => {
-  console.log('SIBI Onboarding RESTful API server started on: ' + port);
-}));
 
-
-
+app.listen(port, 40, callback => {
+  console.log('todo list RESTful API server started on: ' + port);
+});
