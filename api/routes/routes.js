d10d9b6308d52ba9b2e65fe3b4f5773d58e9bfd0
Alex Witkowski <alex@sibipro.com>
2019-02-27 15:39:59 -0700

    added buttons, and a user list for the design team.

--- api/routes/routes.js
+++ api/routes/routes.js
@@ -4,76 +4,114 @@ module.exports = function(app) {
 
 
   app.post('/events/actions', (req, res) => {
-      if(req.actions.selected_option.value == 'design') {
+      req.body.actions.map(val => {
+        if(val.value == 'design') {
+            res.send([
+                {
+                    "type": "section",
+                    "text": {
+                        "type": "mrkdwn",
+                        "text": "*Welcome to the design team!* :art:   :writing_hand:\nHere's a list of members on the team:"
+                    }
+                },
+                {
+                    "type": "section",
+                    "fields": [
+                        {
+                            "type": "mrkdwn",
+                            "text": "<@U8RSUTGHL>"
+                        },
+                        {
+                            "type": "mrkdwn",
+                            "text": "<@UD6P13AP7>"
+                        },
+                        {
+                            "type": "mrkdwn",
+                            "text": "<@UFX11L0HX>"
+                        },
+                        {
+                            "type": "mrkdwn",
+                            "text": "<@UALGA7P40>"
+                        },
+                        {
+                            "type": "mrkdwn",
+                            "text": "<@UAJH71T9A>"
+                        },
+                        {
+                            "type": "mrkdwn",
+                            "text": "<@UAH5FKBTL>"
+                        }
+                    ]
+                }
+            ]);
+        } else {
+          res.send({
+              "title" : "",
+              "body" : "not design"
+          });
+        };
+      })
 
-      };
-
-      if(req.actions.selected_option.value == 'backend') {
-
-    };
-
-    if(req.actions.selected_option.value == 'frontend') {
-
-    };
-
-    if(req.actions.selected_option.value == 'vendor') {
-      
-    };
-      
-  })
+  });
 
   app.post('/events/department', (req, res) => {
-      res.send({
-            "type": "section",
-            "text": {
-                "type": "mrkdwn",
-                "text": "Please select the team you'll be working with at SIBI."
-            },
-            "accessory": {
-                "type": "static_select",
-                "placeholder": {
-                    "type": "plain_text",
-                    "text": "Select an item",
-                    "emoji": true
+      res.send([
+        {
+            "type": "actions",
+            "elements": [
+                {
+                    "type": "button",
+                    "text": {
+                        "type": "plain_text",
+                        "text": "Design",
+                        "emoji": true
+                    },
+                    "value": "design"
                 },
-                "options": [
-                    {
-                        "text": {
-                            "type": "plain_text",
-                            "text": "design",
-                            "emoji": true
-                        },
-                        "value": "design"
+                {
+                    "type": "button",
+                    "text": {
+                        "type": "plain_text",
+                        "text": "Backend",
+                        "emoji": true
                     },
-                    {
-                        "text": {
-                            "type": "plain_text",
-                            "text": "frontend",
-                            "emoji": true
-                        },
-                        "value": "frontend"
+                    "value": "backend"
+                },
+                {
+                    "type": "button",
+                    "text": {
+                        "type": "plain_text",
+                        "text": "Frontend",
+                        "emoji": true
                     },
-                    {
-                        "text": {
-                            "type": "plain_text",
-                            "text": "backend",
-                            "emoji": true
-                        },
-                        "value": "backend"
+                    "value": "frontend"
+                },
+                {
+                    "type": "button",
+                    "text": {
+                        "type": "plain_text",
+                        "text": "Vendor management",
+                        "emoji": true
                     },
-                    {
-                        "text": {
-                            "type": "plain_text",
-                            "text": "vendor management",
-                            "emoji": true
-                        },
-                        "value": "vendor"
-                    }
-                ]
-            }
+                    "value": "vendor"
+                }
+            ]
+        },
+        {
+            "type": "context",
+            "elements": [
+                {
+                    "type": "plain_text",
+                    "text": "Please select the team you'll be working with at SIBI.",
+                    "emoji": true
+                }
+            ]
         }
+    ]
       )
   }); 
+
+  
 
   app.post('/events/department/dev', (req, res) => {
     res.send({
